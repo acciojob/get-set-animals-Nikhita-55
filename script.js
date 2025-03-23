414252e466a5d43d323e9cf0cdcdc1ebcd61f7d5
NIKHITA PARDHE <153489800+Nikhita-55@users.noreply.github.com>
2025-03-23 13:03:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species; // Store species in a private variable
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+// Cat Class - Inherits from Animal
+class Cat extends Animal {
+  constructor() {
+    super('Cat'); // Call the parent class constructor with 'Cat' as species
+  }
+
+  // Method to purr
+  purr() {
+    console.log('purr');
+  }
+}
+
+// Dog Class - Inherits from Animal
+class Dog extends Animal {
+  constructor() {
+    super('Dog'); // Call the parent class constructor with 'Dog' as species
+  }
+
+  // Method to bark
+  bark() {
+    console.log('woof');
+  }
+}
+
+// Example Usage:
+
+// Create instances of Cat and Dog
+const myCat = new Cat();
+const myDog = new Dog();
+
+// Call methods on Cat and Dog instances
+myCat.makeSound(); // Output: The Cat makes a sound
+myCat.purr(); // Output: purr
+
+myDog.makeSound(); // Output: The Dog makes a sound
+myDog.bark(); // Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
